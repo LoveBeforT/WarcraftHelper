400b592aedf8a032b9edfb2c6eddbe2bc9df942b
KouKouChan <1010433454@qq.com>
2022-04-17 21:00:39 +0800

    修改游戏内存中的fps最大值，避免注册表被原数据覆盖

--- WarcraftHelper/unlockfps.cpp
+++ WarcraftHelper/unlockfps.cpp
@@ -3,6 +3,9 @@
 
 bool UnlockFPS_Hooked = false;
 
+DWORD(*GetGameOpt)() = 0;
+DWORD(__fastcall* SetGameOptValue)(DWORD pthis, DWORD unused, DWORD idx, DWORD valuee) = 0;
+
 UnlockFPS::UnlockFPS() {}
 UnlockFPS::~UnlockFPS() {}
 
@@ -26,11 +29,17 @@ void UnlockFPS::Start(DWORD m_GamedllBase, Version m_War3Version) {
 			return;
 		}
 		addr += 2;
+		GetGameOpt = (DWORD(*)())(m_GamedllBase + 0x2A50);
+		SetGameOptValue = (DWORD(__fastcall*)(DWORD, DWORD, DWORD, DWORD))(m_GamedllBase + 0x2CC0);
 		break;
 	case Version::v124e:
+		GetGameOpt = (DWORD(*)())(m_GamedllBase + 0x5720);
+		SetGameOptValue = (DWORD(__fastcall*)(DWORD, DWORD, DWORD, DWORD))(m_GamedllBase + 0x57F0);
 		addr += 0x62DF9B;
 		break;
 	case Version::v127a:
+		GetGameOpt = (DWORD(*)())(m_GamedllBase + 0x23E00);
+		SetGameOptValue = (DWORD(__fastcall*)(DWORD, DWORD, DWORD, DWORD))(m_GamedllBase + 0x25A70);
 		addr += 0x5FCFB;
 		break;
 	default:
@@ -43,7 +52,9 @@ void UnlockFPS::Start(DWORD m_GamedllBase, Version m_War3Version) {
 	dm.dmDriverExtra = 0;
 	EnumDisplaySettings(NULL, ENUM_REGISTRY_SETTINGS, &dm);
 	if (dm.dmDisplayFrequency > 60) {
-		WriteDwordToReg("SOFTWARE\\Blizzard Entertainment\\Warcraft III\\Video", "refreshrate", dm.dmDisplayFrequency);
+		SetGameOptValue(GetGameOpt(), 0, 4, dm.dmDisplayFrequency);	// refreshrate
+		// SetGameOptValue(GetGameOpt(), 0, 22, 200);	// maxfps
+		// WriteDwordToReg("SOFTWARE\\Blizzard Entertainment\\Warcraft III\\Video", "refreshrate", dm.dmDisplayFrequency);
 	}
 
 	unsigned char bytes[] = { 0xFF };
