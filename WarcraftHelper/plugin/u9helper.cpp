61e17981faa5942fa6ff4be7f2fc3c55f2bb26e6
KouKouChan <1010433454@qq.com>
2025-10-06 21:48:27 +0800

    BUGFIX: 修复U9助手插件的函数中once标记位无效问题

--- WarcraftHelper/plugin/u9helper.cpp
+++ WarcraftHelper/plugin/u9helper.cpp
@@ -1,17 +1,16 @@
 #include "u9helper.hpp"
+#include <mutex>
 
 static DWORD u9MapSigAddr = 0;
+static std::once_flag flag;
 
 bool(__fastcall *orgGetMapSignature)(DWORD *ecx, DWORD edx, DWORD unk00, DWORD unk01, DWORD unk02, DWORD unk03, DWORD unk04, DWORD unk05) = 0;
 bool __fastcall GetMapSignature(DWORD *ecx, DWORD edx, DWORD unk00, DWORD unk01, DWORD unk02, DWORD unk03, DWORD unk04, DWORD unk05)
 {
-	static bool once = false;
-	static unsigned char bytes[] = {0x83, 0xF8, 0x04};
-
-	if (!once)
-	{
+	std::call_once(flag, [&] {
+		static unsigned char bytes[] = {0x83, 0xF8, 0x04};
 		Game::PatchMemory(u9MapSigAddr, bytes, 3);
-	}
+    });
 	return orgGetMapSignature(ecx, edx, unk00, unk01, unk02, unk03, unk04, unk05);
 }
 
